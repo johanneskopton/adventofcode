9d2018fc31e82068cfb6f6f32c55955b72e8639c
Johannes Kopton <johannes@kopton.org>
2023-01-03 15:23:09 +0100

    Mayke python script nicer.

--- 2022/python/day01b.py
+++ 2022/python/day01b.py
@@ -1,8 +1,7 @@
 infile = open("data/day01_large.txt", "r")
 data = infile.read()[:-1].split("\n\n")
 data = [a.split("\n") for a in data]
-for i in range(len(data)):
-    data[i] = [int(a) for a in data[i]]
+data = [[int(a) for a in elve] for elve in data]
 elves = sorted([sum(elve) for elve in data])
 print("Part1:\t{}".format(elves[-1]))
 print("Part2:\t{}".format(sum(elves[-3:])))
