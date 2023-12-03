8c9f0fe32542bd76c165272d6c67f71c23fe9174
Johannes Kopton <johannes@kopton.org>
2023-12-03 22:41:21 +0100

    Add stopwatch script.

---
+++ 2023/stopwatch.sh
@@ -0,0 +1,9 @@
+stopwatch() {
+    start=$(date +%s)
+    while true; do
+        time="$(( $(date +%s) - $start))"
+        printf '%s\r' "$(date -u -d "@$time" +%H:%M:%S)"
+        sleep 1
+    done
+}
+stopwatch
